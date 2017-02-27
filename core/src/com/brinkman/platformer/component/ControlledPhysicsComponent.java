81e3b6dbd6a589348d566c25a1e4ea48f5123f0c
Caleb Brinkman <cbrinkman@sonatype.com>
2017-02-26 19:42:08 -0600

    Extract AbstractControlledBody

--- core/src/com/brinkman/platformer/component/ControlledPhysicsComponent.java
+++ core/src/com/brinkman/platformer/component/ControlledPhysicsComponent.java
@@ -1,45 +1,9 @@
 package com.brinkman.platformer.component;
 
-import com.brinkman.platformer.physics.AbstractMotileBody;
-import com.brinkman.platformer.physics.ControlledBody;
+import com.brinkman.platformer.physics.AbstractControlledBody;
 
 /**
  * @author Caleb Brinkman
  */
-public class ControlledPhysicsComponent extends AbstractMotileBody implements ControlledBody, PhysicsComponent {
-    private boolean jumping;
-    private boolean justJumped;
-    private float jumpVelocity;
-    private boolean touchingRightWall;
-    private boolean touchingLeftWall;
-
-    @Override
-    public boolean isJumping() { return jumping; }
-
-    @Override
-    public void setJumping(boolean jumping) { this.jumping = jumping; }
-
-    @Override
-    public boolean justJumped() { return justJumped; }
-
-    @Override
-    public void setJustJumped(boolean justJumped) { this.justJumped = justJumped; }
-
-    @Override
-    public float getJumpVelocity() { return jumpVelocity; }
-
-    @Override
-    public void setJumpVelocity(float jumpVelocity) { this.jumpVelocity = jumpVelocity; }
-
-    @Override
-    public boolean isTouchingRightWall() { return touchingRightWall; }
-
-    @Override
-    public void setTouchingRightWall(boolean touchingRightWall) { this.touchingRightWall = touchingRightWall; }
-
-    @Override
-    public boolean isTouchingLeftWall() { return touchingLeftWall; }
-
-    @Override
-    public void setTouchingLeftWall(boolean touchingLeftWall) { this.touchingLeftWall = touchingLeftWall; }
+public class ControlledPhysicsComponent extends AbstractControlledBody implements PhysicsComponent {
 }
