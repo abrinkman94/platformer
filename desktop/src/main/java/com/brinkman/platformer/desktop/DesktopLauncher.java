331e6fec666ae9481e58fe089509187c8be94eac
Caleb Brinkman <cbrinkman@sonatype.com>
2017-03-04 23:48:53 -0600

    Fix weird-ass package naming

--- desktop/src/main/java/com/brinkman/platformer/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/brinkman/platformer/desktop/DesktopLauncher.java
@@ -1,4 +1,4 @@
-package main.java.com.brinkman.platformer.desktop;
+package com.brinkman.platformer.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
