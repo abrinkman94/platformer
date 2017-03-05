f6a5f61ee58ce381a46dee1de841419fea0dce22
Caleb Brinkman <floralvikings@gmail.com>
2017-03-05 02:31:18 -0600

    Add SimpleStatusComponent class

---
+++ core/src/main/java/com/brinkman/platformer/component/status/SimpleStatusComponent.java
@@ -0,0 +1,6 @@
+package com.brinkman.platformer.component.status;
+
+/**
+ * Extremely simple (read: unchanged) implementaion of {@link AbstractStatusComponent}.
+ */
+public class SimpleStatusComponent extends AbstractStatusComponent { }
